60d7987e89cc234290f95fd205cfd6ba7bc39894
jsjohn100481 <jsjohn@umd.edu>
2024-10-02 16:43:07 -0400

    Final Results w/ Plots

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ml_code.data_load import load_data
 from ml_code.pre_processing import preprocess_data
 from ml_code.models import ModelFactory
 from ml_code.train import train_and_evaluate
+from ml_code.metrics import print_metrics
 
 with open('C:/Users/jsjohn/UMD/LabSession/Lab1/Test/config.json') as config_file:
     config = json.load(config_file)
@@ -16,4 +17,4 @@ model = ModelFactory.get_model(config["model_type"])
 
 accuracy, cm, y_test, y_prob = train_and_evaluate(model, X_train, X_test, y_train, y_test)
 
-#print_metrics(accuracy, cm, y_test, y_prob)
+print_metrics(accuracy, cm, y_test, y_prob)
